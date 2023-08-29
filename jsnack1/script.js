bfdae6159170e949f3eabf3af95aadda8b1c36b3
EliaBuratti <elia.buratti.dev@gmail.com>
2023-08-29 14:40:38 +0200

    complete first snack

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -2,4 +2,15 @@
 L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore. */
 
-console.log('ciao mondo');
+const firstNumber = Number(prompt('inserisci il primo numero'));
+
+const secondNumber = Number(prompt('inserisci il primo numero'));
+
+console.log('first number ', firstNumber, '; second number', secondNumber);
+
+if (firstNumber > secondNumber) {
+    console.log('il numero maggiore è', firstNumber);
+
+} else {
+    console.log('il numero maggiore è', secondNumber);
+};

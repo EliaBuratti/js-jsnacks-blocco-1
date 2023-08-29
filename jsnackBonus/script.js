fd40bd5682aa1344becfdcbfeeb0a2e839b083ef
EliaBuratti <elia.buratti.dev@gmail.com>
2023-08-29 16:05:22 +0200

    add bonus folder

---
+++ jsnackBonus/script.js
@@ -0,0 +1,6 @@
+/* :barretta_cioccolato: :barretta_cioccolato: Snack Bonus:
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array. */
+
+console.log('ciao mondo');

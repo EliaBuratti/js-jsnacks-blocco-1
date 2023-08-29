64f3999a9b83fb9c10ebd8a709059941fa94491f
EliaBuratti <elia.buratti.dev@gmail.com>
2023-08-29 19:05:21 +0200

    fix bonus, prompt (only number control)

--- jsnackBonus/script.js
+++ jsnackBonus/script.js
@@ -11,14 +11,33 @@ for (let i = 0; i < 6; i++) {
     
     //chiedo all'utente di inserire dei numeri e li stampo in console
     const userNum = Number(prompt('Inserisci dei numeri'));
-    console.log(`il ${i + 1}° numero inserito è: ${userNum}`);
 
-    //identifico i numeri dispari e li metto nell'array
-    if (userNum % 2 !== 0) {
-        oddNum.push(userNum);
+    //verifico che ci siano solo numeri 
+    if ((isNaN(userNum)) || (userNum == "") || (userNum == "undefined")) {
+        
+        alert('compila i campi con i numeri');
+        window.location.reload();
+        break;
+
+    } else {
+        console.log(`il ${i + 1}° numero inserito è: ${userNum}`);
+
+        //identifico i numeri dispari e li metto nell'array
+        if (userNum % 2 !== 0) {
+            oddNum.push(userNum);
+        }
     }
+};
+
+//controllo se nell'array sono presenti dei dati e restituisco il risultato
+if (oddNum != 0) {
+
+    //stampo in console l'array aggiornato con solo i numeri dispari
+    console.log(`Array con solo i numeri dispari: ${oddNum}`);
+
+} else {
+    console.log('Non hai inserito numeri dispari');
 
 };
 
-//stampo in console l'array aggiornato con solo i numeri dispari
-console.log(`Array con solo i numeri dispari: ${oddNum}`);
+

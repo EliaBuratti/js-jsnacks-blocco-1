1d63b6cd2d5481c32dac1b83116e7d727f3c219f
EliaBuratti <elia.buratti.dev@gmail.com>
2023-08-29 16:45:03 +0200

    complete bonus snack

--- jsnackBonus/script.js
+++ jsnackBonus/script.js
@@ -3,4 +3,21 @@ Crea un array vuoto.
 Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array. */
 
-console.log('ciao mondo');
+//creo una costante dove andrò ad inserire tutti i numeri dispari
+const oddNum = [];
+
+for (let i = 0; i < 6; i++) {
+    
+    //chiedo all'utente di inserire dei numeri e li stampo in console
+    const userNum = Number(prompt('Inserisci dei numeri'));
+    console.log(`il ${i + 1}° numero inserito è: ${userNum}`);
+
+    //identificoi numeri dispari e li metto nell'array
+    if (userNum % 2 !== 0) {
+        oddNum.push(userNum);
+    }
+
+};
+
+//stampo in console l'array aggiornato con solo i numeri dispari
+console.log(`Array con solo i numeri dispari: ${oddNum}`);
